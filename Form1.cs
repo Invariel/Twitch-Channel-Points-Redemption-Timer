72c5a5af4f860957395a4d8924e22cd01b0e6339
Adam Michaud <invariel@gmail.com>
2023-09-04 17:20:47 -0400

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Twitch_Channel_Points_Redemption_Timer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
